a418c4546b686304ceeabd63e3ba3d128cc0381e
thanhdinhit <thanhdinhit1280@gmail.com>
2023-02-04 20:40:42 +0700

    [Web] Update project chemistry

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -23,7 +23,7 @@ class Main extends Component {
         return (
             <main>
                 <div className="wrapper">
-                    {/* <HeaderComponent /> */}
+                    <HeaderComponent />
                     <Switch>
                         <Route exact path='/' component={HomeComponent} />
                         <Route component={NotFound} />
